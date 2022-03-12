c8e8315640e72d70e1801bcff72ed356dc9c12e6
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2022-03-12 23:29:59 +0530

    Create frontend

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+const PORT = 3000;
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/bookings", (req, res) => {
+  res.render("bookings");
+});
+app.get("/rent", (req, res) => {
+  res.render("rent");
+});
+app.listen(PORT, function (req, res) {
+  console.log("Server is running on port " + PORT);
+});
